e8fd9ffe72d493062e759659f47118e8b12897ca
kalyan reddy <Kalyan.katla@gmail.com>
2019-04-12 12:05:38 -0700

    Create model.py
    lgb_model with optimized hyperparameters.

---
+++ model.py
@@ -0,0 +1,23 @@
+# lgb model.
+
+def run_lgb(train_X, train_y, val_X, val_y, test_X):
+    params = {
+        "objective" : "regression",
+        "metric" : "rmse", 
+        "num_leaves" : 30,
+        "min_child_samples" : 100,
+        "learning_rate" : 0.1,
+        "bagging_fraction" : 0.7,
+        "feature_fraction" : 0.5,
+        "bagging_frequency" : 5,
+        "bagging_seed" : 2018,
+        "verbosity" : -1
+    }
+    
+    lgtrain = lgb.Dataset(train_X, label=train_y)
+    lgval = lgb.Dataset(val_X, label=val_y)
+    model = lgb.train(params, lgtrain, 1000, valid_sets=[lgval], early_stopping_rounds=100, verbose_eval=100)
+    
+    pred_test_y = model.predict(test_X, num_iteration=model.best_iteration)
+    pred_val_y = model.predict(val_X, num_iteration=model.best_iteration)
+    return pred_test_y, model, pred_val_y
